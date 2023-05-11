ec78a4f33f9e18275bae39b7f17372af5486fd75
Kim-Min-Hyeok <kkmin11203@gmail.com>
2023-05-11 18:54:40 +0900

    5/11

---
+++ 5th_homework_김민혁/lib/models/webtoon_model.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class WebtoonModel {
+
+  late final String title, thumb, id;
+
+  WebtoonModel.fromJson(Map<String, dynamic> json) :
+    title = json['title'],
+    thumb = json['thumb'],
+    id = json['id'];
+  
+}
